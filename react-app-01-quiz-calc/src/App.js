01272150d0aaa2b9badeb1ff930ae5448314dbb2
unknown <hh8164@naver.com>
2025-06-08 18:27:52 +0900

    a

--- react-app-01-quiz-calc/src/App.js
+++ react-app-01-quiz-calc/src/App.js
@@ -2,89 +2,65 @@ import { useState } from "react";
 
 function App() {
 
-  let [num1,setNum1] = useState('');
-  let [num2,setNum2] = useState('');
+
+  let [num, setNum] = useState({ num1: "", num2: "" });
+  let [oper, setOper] = useState('');
   let [result, setResult] = useState(null);
 
-  function number1(event){
-    if(num1 === ''){
-      setNum1(event.target.value)
+
+  function number(e) {
+
+
+    if (oper === '') {
+      setNum(prev => ({ ...prev, num1:prev.num1+e.target.value }) )
+    } else if(oper !== '') {
+      setNum(prev => ({ ...prev, num2: prev.num2+e.target.value }))
     }
-    console.log('num1: ',num1)
+
   }
-  function number2(event){
-    if(num1 !== ''){
-      setNum2(event.target.value)
+  function calc() {
+    switch (oper) {
+      case '+':
+        setResult(Number(num.num1) + Number(num.num2))
+        break;
+
+      case '-':
+        setResult(Number(num.num1) - Number(num.num2))
+        break;
+
+      case '*':
+        setResult(Number(num.num1) * Number(num.num2))
+        break;
+
+      case '/':
+        setResult(Number(num.num1) / Number(num.num2))
+        break;
+      default:
+
+        break;
     }
-  }
-  function add(){
-    setResult(num1 + num2)
-  }
-  function subtraction(){
-    setResult(num1 - num2)
-  }
-  function multiple(){
-    setResult(num1 * num2)
-  }
-  function division(){    
-    setResult(num1 / num2)
-  }
-  function reset(){
-    setResult(null)
   }
 
+
+  
+  const numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+  const formula = ["+", "-", "*", "/"]
   return (
     <div className="App">
-      <p>식:{num1}{num2}</p>
+      <p>식: {num.num1}{oper}{num.num2}</p>
       <p>결과: {result}</p>
-      <input type="button" value='1' onClick={(event)=>{
-        number1(event);
-        number2(event);
-      }}></input>
-      <input type="button" value='2' onClick={(event)=>{
-        number1(event);
-        number2(event);
-      }}></input>
-      <input type="button" value='3' onClick={(event)=>{
-        number1(event);
-        number2(event);
-      }}></input>
-      <input type="button" value='4' onClick={(event)=>{
-        number1(event);
-        number2(event);
-      }}></input>
-      <input type="button" value='5' onClick={(event)=>{
-        number1(event);
-        number2(event);
-      }}></input>
-      <input type="button" value='6' onClick={(event)=>{
-        number1(event);
-        number2(event);
-      }}></input>
-      <input type="button" value='7' onClick={(event)=>{
-        number1(event);
-        number2(event);
-      }}></input>
-      <input type="button" value='8' onClick={(event)=>{
-        number1(event);
-        number2(event);
-      }}></input>
-      <input type="button" value='9' onClick={(event)=>{
-        number1(event);
-        number2(event);
-      }}></input>
-      <input type="button" value='0' onClick={(event)=>{
-        number1(event);
-        number2(event);
-      }}></input>
-      <br></br>
-      <input type="button" value='+' onClick={add}></input>
-      <input type="button" value='-' onClick={subtraction}></input>
-      <input type="button" value='*' onClick={multiple}></input>
-      <input type="button" value='/' onClick={division}></input>
-      <br></br>
-      <input type="button" value='=' onClick={result}></input>
-      <input type="button" value='C' onClick={reset}></input>
+
+      {numArray.map((item => <input type="button" value={item} onClick={(e) => number(e)} />))}
+      <br />
+      {formula.map((item => <input type="button" value={item} onClick={(e) => { setOper(e.target.value) }} />))}
+      <br />
+      <input type="button" value='=' onClick={calc} />
+      <input type="button" value='c' onClick={() => {
+        setResult('') 
+        setNum({num1:'' , num2:''})
+        setOper('')
+        }}/>
+
     </div>
   );
 }
